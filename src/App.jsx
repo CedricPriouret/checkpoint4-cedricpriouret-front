a39ea76e4007dc8bf87ddbb94cf87aed7fdfa9f2
Cédric Priouret <cedric.priouret@gmail.com>
2021-02-03 17:09:39 +0100

    Connection succesfull

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./normalize.css";
 // import Header from "./components/Home/Header";
 import Navbar from "./components/Home/Navbar";
 import Main from "./components/Home/Main";
+import Users from "./components/Manage/Users";
 // import Footer from "./components/Home/Footer";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       {/* <Header /> */}
       <Navbar />
       <Main />
+      <Users />
       {/* <Footer /> */}
     </div>
   );
---
+++ src/components/Manage/Users.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./css/style.css";
+import { useState } from "react";
+import axios from "axios";
+
+function Users() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [mail, setMail] = useState("");
+
+  const addUser = () => {
+    axios
+      .post("http://localhost:3001/create", {
+        firstname: firstname,
+        lastname: lastname,
+        age: age,
+        country: country,
+        mail: mail,
+      })
+      .then(() => console.log("success"));
+  };
+
+  return (
+    <div className="container-user">
+      <div className="information">
+        <label>Nom:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setFirstname(event.target.value);
+          }}
+        />
+        <label>Prénom:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setLastname(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Région:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Mail:</label>
+        <input
+          type="email"
+          onChange={(event) => {
+            setMail(event.target.value);
+          }}
+        />
+        <button onClick={addUser}>Ajouter un utilisateur</button>
+      </div>
+    </div>
+  );
+}
+
+export default Users;
